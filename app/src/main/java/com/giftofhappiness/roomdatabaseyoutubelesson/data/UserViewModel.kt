116bd1ec22b23acff4e9d181072d792315736659
jlawrencesha <44271286+jlawrencesha@users.noreply.github.com>
2020-09-04 08:21:41 -0700

    commit before branch switch

--- app/src/main/java/com/giftofhappiness/roomdatabaseyoutubelesson/data/UserViewModel.kt
+++ app/src/main/java/com/giftofhappiness/roomdatabaseyoutubelesson/data/UserViewModel.kt
@@ -1,4 +1,27 @@
 package com.giftofhappiness.roomdatabaseyoutubelesson.data
 
-class UserViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
+
+@InternalCoroutinesApi
+class UserViewModel(application: Application): AndroidViewModel(application) {
+
+    private val readAllData: LiveData<List<User>>
+    private val repository: UserRepository
+    init{
+        val userDao = UserDatabase.getDatabase(application).userDao()
+        repository = UserRepository(userDao)
+        readAllData = repository.readAllData
+    }
+        fun addUser(user: User){
+            viewModelScope.launch(Dispatchers.IO){ this
+            repository.addUser(user)
+            }
+        }
 }
