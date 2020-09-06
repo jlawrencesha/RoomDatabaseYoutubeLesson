126b500929581b8d6941fc06e2df0a0c3463e2e4
jlawrencesha <44271286+jlawrencesha@users.noreply.github.com>
2020-09-05 17:13:34 -0700

    added insert functionality to database

---
+++ app/src/main/java/com/giftofhappiness/roomdatabaseyoutubelesson/fragements/add/AddFragment.kt
@@ -0,0 +1,70 @@
+package com.giftofhappiness.roomdatabaseyoutubelesson.fragements.add
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.giftofhappiness.roomdatabaseyoutubelesson.R
+import com.giftofhappiness.roomdatabaseyoutubelesson.data.User
+import com.giftofhappiness.roomdatabaseyoutubelesson.data.UserViewModel
+import kotlinx.android.synthetic.main.fragment_add.*
+import kotlinx.android.synthetic.main.fragment_add.view.*
+import kotlinx.coroutines.InternalCoroutinesApi
+
+
+class AddFragment : Fragment() {
+
+    @InternalCoroutinesApi
+    private lateinit var mUserViewModel: UserViewModel
+    @InternalCoroutinesApi
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_add, container, false)
+
+
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        view.addButton.setOnClickListener {
+            insertDataToDatabase()
+        }
+
+        return view
+    }
+
+    @InternalCoroutinesApi
+    private fun insertDataToDatabase(){
+
+        val firstName = editTextTextPersonName.text.toString()
+        val lastName = editTextTextPersonName2.text.toString()
+        val age = editTextNumber.text
+
+        if(inputCheck(firstName,lastName,age)){
+
+            val user = User(id=0,firstName,lastName, Integer.parseInt(age.toString()))
+            mUserViewModel.addUser(user)
+
+            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_LONG).show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }else{
+            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+
+    private fun inputCheck(firstName: String, lastName: String, age:Editable ): Boolean{
+        return!(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
+
+
+
+    }
+
+}
